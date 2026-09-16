a04f0d0bbe254c2d7dd1f349523a535dc880481f
Denys Lopez <denys.lopez@gmail.com>
2026-09-16 03:45:45 -0600

    style(denysoft): align typography with current design authority

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,16 +1,9 @@
-import { IBM_Plex_Mono, Manrope, Newsreader } from "next/font/google";
+import { IBM_Plex_Mono, Instrument_Sans } from "next/font/google";
 
-export const displayFont = Newsreader({
+export const sansFont = Instrument_Sans({
   subsets: ["latin"],
   display: "swap",
-  variable: "--font-newsreader",
-  style: ["normal", "italic"],
-});
-
-export const sansFont = Manrope({
-  subsets: ["latin"],
-  display: "swap",
-  variable: "--font-manrope",
+  variable: "--font-instrument-sans",
 });
 
 export const monoFont = IBM_Plex_Mono({
@@ -20,4 +13,4 @@ export const monoFont = IBM_Plex_Mono({
   weight: ["400", "500"],
 });
 
-export const fontVariables = `${displayFont.variable} ${sansFont.variable} ${monoFont.variable}`;
+export const fontVariables = `${sansFont.variable} ${monoFont.variable}`;
